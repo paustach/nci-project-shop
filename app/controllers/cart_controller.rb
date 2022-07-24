cfab04560af4882cee69eb25399b4a2756f1eafe
Paulina Stachera Diaz <stachera.p@gmail.com>
2022-07-24 21:26:21 +0100

    style updates

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -7,22 +7,7 @@ class CartController < ApplicationController
     @product = Product.find_by(id: params[:id])
     quantity = params[:quantity].to_i
     current_orderable = @cart.orderables.find_by(product_id: @product.id)
-    if current_orderable && quantity > 0
-      current_orderable.update(:quantity)
-    elsif quantity <= 0
-      current_orderable.destroy
-    else
-      @cart.orderables.create(product: @product, :quantity)
-    end
-
-    respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: [turbo_stream.replace('cart',
-                                                   partial: 'cart/cart',
-                                                   locals: { cart: @cart }),
-                              turbo_stream.replace(@product)]
-      end
-    end
+    
   end
 
   def remove
@@ -35,5 +20,4 @@ class CartController < ApplicationController
       end
     end
   end
-end
 end
