8c045fcf145ae017e809932f1ec2caeb51aff3ec
Paulina Stachera Diaz <stachera.p@gmail.com>
2022-08-03 00:12:48 +0100

    add/remove options for the cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -15,11 +15,27 @@ class CartController < ApplicationController
       @cart.orderables.create(product: @product, quantity:)
     end
 
-    
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: [turbo_stream.replace('cart',
+                                                   partial: 'cart/cart',
+                                                   locals: { cart: @cart }),
+                              turbo_stream.replace(@product)]
+      end
+    end
+
   end
 
   def remove
     Orderable.find_by(id: params[:id]).destroy
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace('cart',
+                                                  partial: 'cart/cart',
+                                                  locals: { cart: @cart })
+      end
+    end
     
   end
 end
